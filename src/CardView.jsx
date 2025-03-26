270f51b55ac90d09744c1319c2f237c0ae8a97f9
Vaibhav Rana <vaibhavrana8900@gmail.com>
2025-03-26 18:56:06 +0530

    adding code

--- src/CardView.jsx
+++ src/CardView.jsx
@@ -5,10 +5,10 @@ const CardView = ({ images }) => {
     <div className="card-view">
       {images.map((image) => (
         <div key={image.id} className="card">
-          <img src={image.url} alt={image.breeds[0]?.name || "Cat"} height="200" width="200" />
+          <img src={image.url} alt={image.breeds.length > 0 ? image.breeds[0].name : "Cat"} height="200" width="200" />
           <div className="card-info">
-            <h3>{image.breeds[0]?.name || "Unknown Breed"}</h3>
-            <p>{image.breeds[0]?.alt_names || "No alternative names"}</p>
+            <h3>{image.breeds.length > 0 ? image.breeds[0].name : "Unknown Breed"}</h3>
+            <p>{image.breeds.length > 0 ? image.breeds[0].alt_names : "No alternative names"}</p>
           </div>
         </div>
       ))}
--- src/ListView.jsx
+++ src/ListView.jsx
@@ -14,12 +14,13 @@ const ListView = ({ images }) => {
         {images.map((image) => (
           <tr key={image.id} className="list-item">
             <td>
-              <img src={image.url} alt={image.breeds[0]?.name || "Cat"} width="100" height="100" />
+              <img src={image.url} alt={image.breeds.length > 0 ? image.breeds[0].name : "Cat"} width="100" height="100" />
             </td>
-            <td>{image.breeds[0]?.name || "Unknown Breed"}</td>
-            <td>{image.breeds[0]?.alt_names || "No alternative names"}</td>
+            <td>{image.breeds.length > 0 ? image.breeds[0].name : "Unknown Breed"}</td>
+            <td>{image.breeds.length > 0 ? image.breeds[0].alt_names : "No alternative names"}</td>
           </tr>
         ))}
+       
       </tbody>
     </table>
   );
